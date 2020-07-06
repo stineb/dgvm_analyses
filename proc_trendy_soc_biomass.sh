0896a5d7c1d2395d5f509a8c105432bb0761bfce
stineb <b.stocker@imperial.ac.uk>
2020-07-06 13:39:01 +0200

    added code to do aboveground biomass vs. soil C analysis

--- proc_trendy_soc_biomass.sh
+++ proc_trendy_soc_biomass.sh
@@ -9,16 +9,16 @@
 ## 2. Directory path
 # For example: dir="/cluster/home/bestocke/data/trendy/v8/"
 
-## select years
-cdo -O selyear,1700/1709 ${2}${1}.nc ${2}/processed/${1}_INIT.nc
+## select years at simulation start
+cdo -O selyear,1701/1710 ${2}${1}.nc ${2}/processed/${1}_INIT.nc
 
-## take mean across years
+## take mean across years at simulation start
 rm ${2}/processed/${1}_INIT_MEAN.nc
 cdo -O timmean ${2}/processed/${1}_INIT.nc ${2}/processed/${1}_INIT_MEAN.nc
 
-## select years
+## select years at simulation end
 cdo -O selyear,2009/2018 ${2}${1}.nc ${2}/processed/${1}_FINAL.nc
 
-## take mean across years
+## take mean across years at simulation end
 rm ${2}/processed/${1}_FINAL_MEAN.nc
 cdo -O timmean ${2}/processed/${1}_FINAL.nc ${2}/processed/${1}_FINAL_MEAN.nc
