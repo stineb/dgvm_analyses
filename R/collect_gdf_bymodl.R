3d3da8a1c1d1e1e502f64098d1c565dd03ad533e
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-10-09 12:14:23 +0200

    separate aboveground-B for TRENDY v7

--- R/collect_gdf_bymodl.R
+++ R/collect_gdf_bymodl.R
@@ -1,9 +1,9 @@
 collect_gdf_bymodl <- function(modl, dir, 
                                filn_cSoil_init, filn_cSoil_final, 
                                filn_cVeg_init, filn_cVeg_final, 
-                               # filn_cLeaf_init, filn_cLeaf_final, 
-                               # filn_cRoot_init, filn_cRoot_final, 
-                               # filn_cWood_init, filn_cWood_final, 
+                               filn_cLeaf_init, filn_cLeaf_final,
+                               filn_cRoot_init, filn_cRoot_final,
+                               filn_cWood_init, filn_cWood_final,
                                filn_npp_init, filn_npp_final,
                                filn_cSoil_change
                                ){
@@ -39,54 +39,54 @@ collect_gdf_bymodl <- function(modl, dir,
       by = c("lon", "lat")
     ) %>%
     
-    # ## cLeaf
-    # left_join(
-    #   read_nc_onefile(paste0(dir, "/processed/", filn_cLeaf_init, ".nc"), ignore_time = TRUE) %>% 
-    #     nc_to_df(varnam = "cLeaf") %>% 
-    #     tidyr::drop_na(myvar) %>% 
-    #     dplyr::rename(cleaf_init = myvar),
-    #   by = c("lon", "lat")
-    # ) %>%
-    # left_join(
-    #   read_nc_onefile(paste0(dir, "/processed/", filn_cLeaf_final, ".nc"), ignore_time = TRUE) %>% 
-    #     nc_to_df(varnam = "cLeaf") %>% 
-    #     tidyr::drop_na(myvar) %>% 
-    #     dplyr::rename(cleaf_final = myvar),
-    #   by = c("lon", "lat")
-    # ) %>%
-    # 
-    # ## cRoot
-    # left_join(
-    #   read_nc_onefile(paste0(dir, "/processed/", filn_cRoot_init, ".nc"), ignore_time = TRUE) %>% 
-    #     nc_to_df(varnam = "cRoot") %>% 
-    #     tidyr::drop_na(myvar) %>% 
-    #     dplyr::rename(croot_init = myvar),
-    #   by = c("lon", "lat")
-    # ) %>%
-    # left_join(
-    #   read_nc_onefile(paste0(dir, "/processed/", filn_cRoot_final, ".nc"), ignore_time = TRUE) %>% 
-    #     nc_to_df(varnam = "cRoot") %>% 
-    #     tidyr::drop_na(myvar) %>% 
-    #     dplyr::rename(croot_final = myvar),
-    #   by = c("lon", "lat")
-    # ) %>%
-    # 
-    # ## cWood
-    # left_join(
-    #   read_nc_onefile(paste0(dir, "/processed/", filn_cWood_init, ".nc"), ignore_time = TRUE) %>% 
-    #     nc_to_df(varnam = "cWood") %>% 
-    #     tidyr::drop_na(myvar) %>% 
-    #     dplyr::rename(cwood_init = myvar),
-    #   by = c("lon", "lat")
-    # ) %>%
-    # left_join(
-    #   read_nc_onefile(paste0(dir, "/processed/", filn_cWood_final, ".nc"), ignore_time = TRUE) %>% 
-    #     nc_to_df(varnam = "cWood") %>% 
-    #     tidyr::drop_na(myvar) %>% 
-    #     dplyr::rename(cwood_final = myvar),
-    #   by = c("lon", "lat")
-    # ) %>%
-    
+    ## cLeaf
+    left_join(
+      read_nc_onefile(paste0(dir, "/processed/", filn_cLeaf_init, ".nc"), ignore_time = TRUE, varnam = "cLeaf") %>%
+        nc_to_df(varnam = "cLeaf") %>%
+        tidyr::drop_na(myvar) %>%
+        dplyr::rename(cleaf_init = myvar),
+      by = c("lon", "lat")
+    ) %>%
+    left_join(
+      read_nc_onefile(paste0(dir, "/processed/", filn_cLeaf_final, ".nc"), ignore_time = TRUE, varnam = "cLeaf") %>%
+        nc_to_df(varnam = "cLeaf") %>%
+        tidyr::drop_na(myvar) %>%
+        dplyr::rename(cleaf_final = myvar),
+      by = c("lon", "lat")
+    ) %>%
+
+    ## cRoot
+    left_join(
+      read_nc_onefile(paste0(dir, "/processed/", filn_cRoot_init, ".nc"), ignore_time = TRUE, varnam = "cRoot") %>%
+        nc_to_df(varnam = "cRoot") %>%
+        tidyr::drop_na(myvar) %>%
+        dplyr::rename(croot_init = myvar),
+      by = c("lon", "lat")
+    ) %>%
+    left_join(
+      read_nc_onefile(paste0(dir, "/processed/", filn_cRoot_final, ".nc"), ignore_time = TRUE, varnam = "cRoot") %>%
+        nc_to_df(varnam = "cRoot") %>%
+        tidyr::drop_na(myvar) %>%
+        dplyr::rename(croot_final = myvar),
+      by = c("lon", "lat")
+    ) %>%
+
+    ## cWood
+    left_join(
+      read_nc_onefile(paste0(dir, "/processed/", filn_cWood_init, ".nc"), ignore_time = TRUE, varnam = "cWood") %>%
+        nc_to_df(varnam = "cWood") %>%
+        tidyr::drop_na(myvar) %>%
+        dplyr::rename(cwood_init = myvar),
+      by = c("lon", "lat")
+    ) %>%
+    left_join(
+      read_nc_onefile(paste0(dir, "/processed/", filn_cWood_final, ".nc"), ignore_time = TRUE, varnam = "cWood") %>%
+        nc_to_df(varnam = "cWood") %>%
+        tidyr::drop_na(myvar) %>%
+        dplyr::rename(cwood_final = myvar),
+      by = c("lon", "lat")
+    ) %>%
+
     ## NPP
     left_join(
       read_nc_onefile(paste0(dir, "/processed/", filn_npp_init, ".nc"), ignore_time = TRUE, varnam = "npp") %>%
@@ -105,6 +105,7 @@ collect_gdf_bymodl <- function(modl, dir,
       by = c("lon", "lat")
     ) %>%
     
+    ## soil C change during the last decade
     left_join(
       read_nc_onefile(paste0(dir, "/processed/", filn_cSoil_change, ".nc"), ignore_time = TRUE, varnam = "cSoil") %>% 
         nc_to_df(varnam = "cSoil") %>% 
